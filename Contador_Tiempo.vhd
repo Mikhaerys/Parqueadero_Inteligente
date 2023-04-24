1fd2d40134ff644fe47121a4bd6dff610d2263bd
Tenshi <79948956+Mikhaerys@users.noreply.github.com>
2023-04-23 19:23:56 -0500

    Mejora en contador y señales de salida
    Se mejoro la salida del contador para que salgan las unidades y decenas como enteros directamente. Además se finalizo los port map incluyendo las señales de salida.

--- Contador_Tiempo.vhd
+++ Contador_Tiempo.vhd
@@ -4,14 +4,15 @@ use IEEE.numeric_std.ALL;
 
 entity Contador_Tiempo is
     Port ( clk, reset, enable, load : in STD_LOGIC;
-           count_out : out STD_LOGIC_VECTOR (5 downto 0));
+           count_out_D : out integer;
+           count_out_U : out integer);
 end Contador_Tiempo;
 
 architecture Behavioral of Contador_Tiempo is
 
     signal count_reg : integer range 0 to 35;
     signal count_reg_1 : integer range 0 to 63;
-    signal evento : integer := 0;
+    signal evento : integer <= 0;
     
 begin
 
@@ -37,6 +38,7 @@ begin
                 
                             if (count_reg = 0) then
                                 count_reg <= data;
+                                evento <= 1
                 
                             else
                                 count_reg <= count_reg - 1;
@@ -60,7 +62,10 @@ begin
                     end if;
                 end if;
             end if;
-            count_out <= std_LOGIC_VECTOR(to_unsigned(count_reg, 6));
+            
+            count_out_D <= count_reg / 10;
+            count_out_U <= count_reg mod 10;
+
         end if;
         
         if evento = 1 then
@@ -100,7 +105,8 @@ begin
                     end if;
             end if;
 
-            count_out <= std_LOGIC_VECTOR(to_unsigned(count_reg_1, 6));
+            count_out_D <= count_reg_1 / 10;
+            count_out_U <= count_reg_1 mod 10;
         end if;
     end process;
 
--- Parquero_Inteligente.vhd
+++ Parquero_Inteligente.vhd
@@ -5,63 +5,85 @@ library ieee ;
 entity Parquero_Inteligente is
   port (
     parqueadero : in std_logic_vector(7 downto 0);
-	clock : in std_logic;
-
+	clock 		: in std_logic;
+	Display_D	: std_logic_vector(6 downto 0);
+	Display_U	: std_logic_vector(6 downto 0);
+	Numero		: std_logic_vector(6 downto 0)
 	) ;
 end Parquero_Inteligente ; 
 
 architecture arch of Parquero_Inteligente is
 
-	signal rst : integer := 1;
-	signal ld : integer := 1;
+	signal rst : STD_LOGIC <= '1';
+	signal ld : STD_LOGIC <= '1';
 
+	signal S1_D : integer;
+	signal S2_D : integer;
+	signal S3_D : integer;
+	signal S4_D : integer;
+	signal S5_D : integer;
+	signal S6_D : integer;
+	signal S7_D : integer;
+	signal S8_D : integer;
 
+	signal S1_U : integer;
+	signal S2_U : integer;
+	signal S3_U : integer;
+	signal S4_U : integer;
+	signal S5_U : integer;
+	signal S6_U : integer;
+	signal S7_U : integer;
+	signal S8_U : integer;
 
 
 	component Contador_Tiempo
 		Port ( clk, reset, enable, load : in STD_LOGIC;
-        		count_out : out STD_LOGIC_VECTOR (5 downto 0));
+			count_out_D : out integer;
+			count_out_U : out integer);
 	end component;
 
 begin
 
 	--Vehiculo 1
 	process(parqueadero(0))
-		P1 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(0), load => ld, count_out =>)
+		P1 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(0), load => ld, count_out_D => S1_D,count_out_U => S1_U)
 	end process;
 	
 	--Vehiculo 2
 	process(parqueadero(1))
-		P2 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(1), load => ld, count_out =>)
+		P2 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(1), load => ld, count_out_D => S2_D,count_out_U => S2_U)
 	end process;
 	
 	--Vehiculo 3
 	process(parqueadero(2))
-	P2 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(2), load => ld, count_out =>)
+		P3 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(2), load => ld, count_out_D => S3_D,count_out_U => S3_U)
 	end process;
 	
 	--Vehiculo 4
 	process(parqueadero(3))
-	P4 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(3), load => ld, count_out =>)
+		P4 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(3), load => ld, count_out_D => S4_D,count_out_U => S4_U)
 	end process;
 	
 	--Vehiculo 5
 	process(parqueadero(4))
-	P5 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(4), load => ld, count_out =>)
+		P5 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(4), load => ld, count_out_D => S5_D,count_out_U => S5_U)
 	end process;
 	
 	--Vehiculo 6
 	process(parqueadero(5))
-	P6 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(5), load => ld, count_out =>)
+		P6 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(5), load => ld, count_out_D => S6_D,count_out_U => S6_U)
 	end process;
 	
 	--Vehiculo 7
 	process(parqueadero(6))
-	P7 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(6), load => ld, count_out =>)
+		P7 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(6), load => ld, count_out_D => S7_D,count_out_U => S7_U)
 	end process;
 	
 	--Vehiculo 8
 	process(parqueadero(7))
-	P8 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(7), load => ld, count_out =>)
+		P8 : Contador_Tiempo port map (clk => clock, reset => rst, enable => parqueadero(7), load => ld, count_out_D => S8_D,count_out_U => S8_U)
 	end process;
+
+
+
 end architecture ;
